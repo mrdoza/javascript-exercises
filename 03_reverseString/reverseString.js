cdaefbf9c667fc7180546ec02ba46962f8fc8e2b
mrdoza <marc.mendoza@gmail.com>
2024-05-15 19:37:38 -0500

    ok - now I understand that it's 1 block of code that needs to pass all the tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -5,35 +5,3 @@ const reverseString = function(word) {
     } 
     return string;
 };
-reverseString("hello");
-
-const reverseString2 = function(word) {
-    string = "";
-    for (let i = word.length -1; i >= 0; i--) {
-        string += word.charAt(i);
-    } 
-    return string;
-};
-reverseString2("hello there");
-
-const reverseString3 = function(word) {
-    string = "";
-    for (let i = word.length -1; i >= 0; i--) {
-        string += word.charAt(i);
-    } 
-    return string;
-};
-reverseString3('123! abc! Hello, Odinite.');
-
-
-const reverseString4 = function(word) {
-    string = "";
-    for (let i = word.length -1; i >= 0; i--) {
-        string += word.charAt(i);
-    } 
-    return string;
-};
-reverseString4('');
-
-// Do not edit below this line
-module.exports = reverseString;
