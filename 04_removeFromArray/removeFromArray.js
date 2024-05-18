b20faa86af41c3bf63e0fdcaebd367c54e4fb6c3
mrdoza <marc.mendoza@gmail.com>
2024-05-18 18:53:33 -0500

    Google is my friend

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (workArray, ...elementsToRemove) {
+  const newArray = workArray.filter(
+    (element) => !elementsToRemove.includes(element)
+  );
+  return newArray;
 };
+//removeFromArray([1, 2, 3, 4], 3, 4);
 
+// const result = removeFromArray([1, 2, 3, 4], 3, 4);
+//console.log(result);
+
+// .shift the first
+//
 // Do not edit below this line
+
 module.exports = removeFromArray;
