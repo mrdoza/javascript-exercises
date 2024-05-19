08303a66bdcf082ee1aeb4f87a3e9ffc04551dbb
mrdoza <marc.mendoza@gmail.com>
2024-05-19 11:31:29 -0500

    smooth

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,37 @@
-const add = function() {
+const add = function(a,b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(array) {
+  const total = array.reduce((acc, current) => acc + current, 0);
+    return total;
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const multiply = function(array) {
+  const total = array.reduce((acc, current) => acc * current, 1);
+    return total;
 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(a) {
+  if (a === 0 ) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i  = a; i > 0; i--) {
+      result *= i;
+    }
+    return result;
+}};
 
 // Do not edit below this line
 module.exports = {
