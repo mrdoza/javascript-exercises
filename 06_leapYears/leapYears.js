a1394fe9f6046029c49c08a4db3c7d98621d8eec
mrdoza <marc.mendoza@gmail.com>
2024-05-18 21:23:02 -0500

    I took the long way. My cade was easier to understand

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 !== 0) {
+    return false;
+  } else if (year % 100 !== 0) {
+    return true;
+  } else if (year % 400 !== 0) {
+    return false;
+  } else {
+    return true;
+  }
 };
+
+const leap = leapYears(2000);
+console.log(leap);
 
 // Do not edit below this line
 module.exports = leapYears;
