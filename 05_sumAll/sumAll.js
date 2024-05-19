c14fe7a3edd2173378d02db3e13aec073fec6da7
mrdoza <marc.mendoza@gmail.com>
2024-05-18 20:17:26 -0500

    Summed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sum = 0;
+  if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  } else {
+    if (a > b) {
+      while (b <= a) {
+        sum += b++;
+      }
+      return sum;
+    } else {
+      while (a <= b) {
+        sum += a++;
+      }
+      return sum;
+    }
+  }
 };
 
+//const result = sumAll(3, -1);
+//console.log(result);
 // Do not edit below this line
 module.exports = sumAll;
