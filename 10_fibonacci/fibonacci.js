309e8a661cc065a4132f9141a532de49d5da031b
mrdoza <marc.mendoza@gmail.com>
2024-05-19 13:01:12 -0500

    crushed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(sequencePosition) {
+        if (sequencePosition <= 0 ) {
+        return "OOPS";
+    };
+        if (sequencePosition = 0){
+            return 0;
+        }
+    const n = parseInt(sequencePosition, 10);
+    let a = 0;
+    let b = 1;
+    for (let i = 2; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 };
+
+const result = fibonacci(6);
+console.log(result);
 
 // Do not edit below this line
 module.exports = fibonacci;
