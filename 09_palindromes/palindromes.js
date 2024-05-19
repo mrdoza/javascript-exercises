0042ff64351a6ad8e14e89b477fc55a7ded7218d
mrdoza <marc.mendoza@gmail.com>
2024-05-19 12:05:54 -0500

    whoosh

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
-};
+    const cleanStr = str.replace(/[^a-z0-9]/gi, "").toLowerCase();
+    const reversedStr = cleanStr.split("").reverse().join("");
+    return cleanStr === reversedStr;
+
+}
 
 // Do not edit below this line
 module.exports = palindromes;
