88c96ce1c175ba116d382989d54aa620f8649974
mrdoza <marc.mendoza@gmail.com>
2024-05-18 22:03:28 -0500

    movin along

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fDeg) {
+  let cel = (fDeg - 32) * 5 / 9;
+  const roundedNum = Math.round(cel * 10) / 10;
+  return roundedNum;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cDeg) {
+  let feh = (cDeg * 9) / 5 + 32;
+  const roundedNum = Math.round(feh * 10) / 10;
+  return roundedNum;
 };
+
+const result = convertToCelsius(100);
+console.log(result);
+
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
+
