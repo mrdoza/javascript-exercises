0bac74893be285e33b2f78a431b004b961e95b75
marc.mendoza <marc.mendoza@gmail.com>
2024-05-20 00:49:02 +0000

    Coded in idx.google

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
 
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+const getTheTitles = function(books) {
+    const titles = books.map(book => book.title);
+        return titles;
 };
+
+// const test = getTheTitles(books);
+// console.log(test);
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
